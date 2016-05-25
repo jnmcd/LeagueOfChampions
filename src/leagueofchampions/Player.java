10eda5295c25b0852a3bc6fce99d89f6f8d8190c
hcps-mcdoweljn <hcps-mcdoweljn@180-18MCDOWELJN.hcps.internal>
2016-05-25 09:43:44 -0400

    Add files via upload

--- src/leagueofchampions/Player.java
+++ src/leagueofchampions/Player.java
@@ -7,11 +7,10 @@ import java.util.ArrayList;
 
 public class Player extends InputAdapter {
     Map m = new Map();
-    Point location = new Point(0, 0);
+    Point location = new Point(30, 30);
     Point destination = null;
     int maxMS = 100;
     int currMS = 0;
-    int angle = 0;
     int gold = 500;
     int ap = 0;
     int ad = 50;
@@ -26,16 +25,30 @@ public class Player extends InputAdapter {
         if(destination != null)
             if(destination.equalTo(location))
                 destination = null;
-            else ;
+            else {
+                double angle = ((Math.atan2(destination.y - location.y, destination.x - location.x) * 180.0 / Math.PI) + 720) % 360;
+                angle = Math.round(angle / 90.0) % 4;
+                if(angle == 0)
+                    location.x++;
+                if(angle == 1)
+                    location.y++;
+                if(angle == 2)
+                    location.x--;
+                if(angle == 3)
+                    location.y--;
+            }
     }
     public void draw(Graphics2D g){
         champ.draw(location.x, location.y, g);
     }
     public void setDestination(Point p){
-        p.x += location.x;
-        p.y += location.y;
-        if(m.pathExists(location, p))
+        p.x += location.x - 350;
+        p.y += location.y - 350;
+        if(m.pathExists(location, p)){
+            System.out.println("Set New Destination");
+            System.out.println("X: " + p.x + ", Y: " + p.y);
             destination = p;
+        }
     }
     @Override public void keyPressed(KeyEvent e){
         if(!keysDown.contains(e.getKeyChar()))
@@ -46,5 +59,7 @@ public class Player extends InputAdapter {
             keysDown.remove(Character.valueOf(e.getKeyChar()));
     }
     @Override public void mousePressed(MouseEvent e){
+        if(e.getButton() == 1)
+            setDestination(new Point(e.getX() - 10, e.getY() - 30));
     }
 }
