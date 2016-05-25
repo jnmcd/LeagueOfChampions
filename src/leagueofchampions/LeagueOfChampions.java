1376089b7b2a11e1ab8f2e84221530244667cb9d
hcps-mcdoweljn <hcps-mcdoweljn@180-18MCDOWELJN.hcps.internal>
2016-05-25 09:46:36 -0400

    Add files via upload

--- src/leagueofchampions/LeagueOfChampions.java
+++ src/leagueofchampions/LeagueOfChampions.java
@@ -1,8 +1,12 @@
 package leagueofchampions;
 
 public class LeagueOfChampions {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         Window w = new Window();
         w.init();
+        for(;;){
+            Thread.sleep(10);
+            w.updateView();
+        }
     }
 }
